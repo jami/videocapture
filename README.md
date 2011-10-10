6abdd9fd1801367f1036318700636a5768ade181
jami <sejamich@googlemail.com>
2011-10-11 01:32:17 +0200

    readme

---
+++ README.md
@@ -0,0 +1,26 @@
+#VidoCapture
+
+##Getting started:
+
+This is the almighty videocapturing - processing - i dont really know program.
+It captures camera images and process them.  
+
+###Linux users:
+
+- Requirements: Gtk+, gcc, gdk, v4l2 
+- Clone the git repo:
+    git clone git://github.com/jami/videocapture.git
+- make
+- ./videocapture
+
+### Known issues:
+
+### Future:
+- Some edge detection 
+- Some funny stuff 
+
+
+
+
+
+
--- src/main.c
+++ src/main.c
@@ -1,3 +1,28 @@
+/***************************************************************************
+ *   videocapture Version 0.1                                              *
+ *   Copyright (C) 2011 by Jan Michalowsky                                 *
+ *   sejamich@googlemail.com                                               *
+ *                                                                         *
+ *   based on V4L2 Specification, Appendix B: Video Capture Example        *
+ *   (http://v4l2spec.bytesex.org/spec/capture-example.html)               *
+ *   contributor: Tobias Müller Tobias_Mueller@twam.info v4l2grab          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
 #include <stdio.h>
 #include "window.h"
 #include "video.h"
